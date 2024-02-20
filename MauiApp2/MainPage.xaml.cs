ec1f7bdc4a6a81d2a75718b857ca05ac295d5d96
Robin van Ee <robin@speak.nl>
2024-02-20 09:29:18 +0100

    ios test

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -6,6 +6,13 @@
         public MainPage()
         {
             InitializeComponent();
+
+#if MACCATALYST || IOS                   
+            if (webView is WebKit.WKWebView wkWebView)
+            {
+                wkWebView.Configuration.Preferences.SetValueForKey(Foundation.NSObject.FromObject(true), (Foundation.NSString)"allowFileAccessFromFileURLs");
+            }
+#endif
         }
 
     }
